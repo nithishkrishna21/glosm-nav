a8ff05d37645a7ac1908fcf67a43d232ea56e1a9
nithishkrishna <nithishkrishna2002@gmail.com>
2026-01-28 15:58:30 -0500

    Policy Update

--- vlfm/policy/__init__.py
+++ vlfm/policy/__init__.py
@@ -1,1 +1,4 @@
 # Copyright (c) 2023 Boston Dynamics AI Institute LLC. All rights reserved.
+
+from .habitat_policies import HabitatITMPolicyV2, HabitatMixin
+from .object_centric_policy import ObjectCentricPolicy
