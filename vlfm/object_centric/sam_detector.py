f7c6def1e6cfa9616866d4d23c3df118aeea0f2a
nithishkrishna <nithishkrishna2002@gmail.com>
2026-01-28 15:37:42 -0500

    MobileSAM Param Config FiX

--- vlfm/object_centric/sam_detector.py
+++ vlfm/object_centric/sam_detector.py
@@ -33,7 +33,18 @@ class SAMDetector:
         mobile_sam = sam_model_registry[model_type](checkpoint=sam_checkpoint)
         mobile_sam.to(device=device)
         mobile_sam.eval()
-        self.mask_generator = SamAutomaticMaskGenerator(mobile_sam)
+
+        # self.mask_generator = SamAutomaticMaskGenerator(mobile_sam)
+
+        self.mask_generator = SamAutomaticMaskGenerator(
+            model=mobile_sam,
+            points_per_side=32,
+            pred_iou_thresh=0.86,
+            stability_score_thresh=0.92,
+            crop_n_layers=1,
+            crop_n_points_downscale_factor=2,
+            min_mask_region_area=100
+        )
 
     def segment_image(self, image: np.ndarray) -> List[Dict]:
         """Segments the objects in the given image.
